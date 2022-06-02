14f14c5460718d0322597b71b3d6abb942b182dc
Ignacio Romero Barolin <nachorb2001@gmail.com>
2022-06-01 22:05:41 -0300

    Tienda informática desde cpu principal

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  )
+}
+
+export default App
